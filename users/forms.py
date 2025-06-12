7e77f23df76c758c88b02f26b9a499e0a36bc017
Andrey Egorov <newart525@yandex.ru>
2025-06-12 17:43:52 +0300

    trubles with create super user

---
+++ users/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+# Форма регистрации
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(max_length=50, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ['email', 'username', 'password1', 'password2' ]
+        labels = {'email' : 'E-mail',
+                  'username' : 'Имя пользователя',
+                  'password1' : 'Пароль',
+                  'password2' : 'Подтверждение пароля',
+                  }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Email'})
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Имя пользователя'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Пароль'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Повторите пароль'})
+
+
+class CustomUserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username']
+        REQUIRED_FIELDS = []
+
+    def __init__(self, *args, **kwargs):
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Email'})
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Имя пользователя'})
+
+
+class CustomUserLoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    def __init__(self, *args, **kwargs):
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Email'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Пароль'})
+
+    def form_valid(self, form):
+        if self.email_verified == True:
+            return self.email
+        else:
+            raise form.ValidationError('Необходимо подтвердить email. Ссылка была отправлена на вашу почту при регистрации')
